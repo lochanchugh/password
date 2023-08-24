e534b10338eb6b335c449fdf186eb9e3b6e1d3a7
lochanchugh <98405764+lochanchugh@users.noreply.github.com>
2023-08-24 09:15:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function checkPassword() {
+            var password = document.getElementById("password").value;
+            if (password === "divya123") {
+                document.getElementById("login-form").style.display = "none";
+                document.getElementById("content").style.display = "block";
+            } else {
+                alert("Incorrect password bro.");
+            }
+        }

2065e082112749e380b0b6888598174a74581be0
lochanchugh <98405764+lochanchugh@users.noreply.github.com>
2023-08-24 09:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
             var password = document.getElementById("password").value;
-            if (password === "divya123") {
+            if (password === "12345678") {
                 document.getElementById("login-form").style.display = "none";
                 document.getElementById("content").style.display = "block";
             } else {
